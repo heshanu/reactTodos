1cbd8e7ed00ce1328e7b05cd674c90839796afa4
heshanu <heshanu97@gmail.com>
2023-05-04 17:34:41 +0530

    Updates

---
+++ precache-manifest.b8fa67c1d236150d0b75caaa9081422f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b5d7d28f1689c6cdc9c",
+    "url": "/heshanu/reactTodos/static/css/main.4ea65742.chunk.css"
+  },
+  {
+    "revision": "8b5d7d28f1689c6cdc9c",
+    "url": "/heshanu/reactTodos/static/js/main.8b5d7d28.chunk.js"
+  },
+  {
+    "revision": "fa677b773aaf13c72ca8",
+    "url": "/heshanu/reactTodos/static/js/1.fa677b77.chunk.js"
+  },
+  {
+    "revision": "7ce06f1d741e10f558ff",
+    "url": "/heshanu/reactTodos/static/js/2.7ce06f1d.chunk.js"
+  },
+  {
+    "revision": "40b4fb810d78553a906e",
+    "url": "/heshanu/reactTodos/static/js/runtime~main.40b4fb81.js"
+  },
+  {
+    "revision": "5944d5fe10a3d8e006a5a25837178a2b",
+    "url": "/heshanu/reactTodos/index.html"
+  }
+];
